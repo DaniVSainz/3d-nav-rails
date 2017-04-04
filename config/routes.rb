048539d8ebac8cd35508c569f0ae5e5b21fcf032
DaniVSainz <danivsainz@gmail.com>
2017-04-04 08:36:24 -0400

    Add images and video, create video test page, add code css for full page video

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :pages
   root 'pages#index'
   get 'tester' => 'pages#tester'
+  get 'video' => 'pages#video'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
