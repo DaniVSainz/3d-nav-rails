15dde98cf2079e60f3ee79420daef4202d2ce76d
DaniVSainz <danivsainz@gmail.com>
2017-04-05 17:37:49 -0400

    Changed routes,rename index to projects, css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pages
-  root 'pages#index'
-  get 'tester' => 'pages#tester'
+  root 'pages#video'
+  get 'projects' => 'pages#projects'
   get 'video' => 'pages#video'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
