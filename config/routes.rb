3d9b24db6ddcffaaaee7123c840f9f6b96ae6791
DaniVSainz <danivsainz@gmail.com>
2017-04-07 21:37:16 -0400

    Create Dani Viewga gs bg bg

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'projects' => 'pages#projects'
   get 'tester' => 'pages#tester'
+  get 'dani' => 'pages#dani'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
